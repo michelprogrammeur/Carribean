2075122870c3c9126558ea132b71232e4a6ce43c
gbn <g_baby972@outlook.com>
2016-03-23 05:21:44 +0100

    ajout  client id   accestoken

--- config/InstagramConfig.php
+++ config/InstagramConfig.php
@@ -1,8 +1,5 @@
 <?php
-/* 	NOTES
- *  Jika hanya menampilkan data pribadi saja , cukup masukan userId dan accesToken
- *  Jika diperlukan autentikasi login semua nya wajib di isi 
- */
+
 	
 	return [
 		'userId' => 'user-id-kamu',
